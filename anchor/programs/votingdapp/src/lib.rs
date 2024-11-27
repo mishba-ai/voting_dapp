7116247bd237441f16495fc775f97667e61fbf7f
mishba-ai <mishbabagban@gmail.com>
2024-11-27 13:18:51 +0530

    initialize setup for the dapp

---
+++ anchor/programs/votingdapp/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
+
+#[program]
+pub mod votingdapp {
+    use super::*;
+}
---
+++ anchor/target/types/votingdapp.ts
@@ -0,0 +1,16 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/votingdapp.json`.
+ */
+export type Votingdapp = {
+  "address": "AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ",
+  "metadata": {
+    "name": "votingdapp",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": []
+};
